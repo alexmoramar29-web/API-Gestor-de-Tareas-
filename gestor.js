d99f79bca5eb382713b39e1b2aa9e3475c5837b5
alexmoramar29-web <alexmoramar29@gmail.com>
2026-03-27 15:31:54 -0600

    Modifique el javascript, y le quite lo que tenia de html para que estubiera separado

--- gestor.js
+++ gestor.js
@@ -3,18 +3,23 @@ const url = "http://127.0.0.1:8000/tareas";
 async function cargar() {
     const res = await fetch(url);
     const tareas = await res.json();
+    
     const lista = document.getElementById("lista");
+    const plantilla = document.getElementById("plantilla-tarea");
+    
     lista.innerHTML = "";
     
     tareas.forEach(t => {
-        let estado = t.completada ? "✅ Completada" : "⏳ Pendiente";
-        lista.innerHTML += `
-            <li>
-                <strong>${t.materia}</strong> - ${t.titulo} <br>
-                <small><em>Estado: ${estado}</em></small> <br><br>
-                <button onclick="cambiarEstado(${t.id}, '${t.titulo}', '${t.materia}', ${t.completada})" style="margin-bottom: 5px; background-color: #555;">Cambiar Estado</button>
-                <button onclick="eliminar(${t.id})" style="background-color: #d9534f;">Eliminar</button>
-            </li>`;
+        const clon = plantilla.content.cloneNode(true);
+
+        clon.querySelector(".tarea-materia").textContent = t.materia;
+        clon.querySelector(".tarea-titulo").textContent = t.titulo;
+        clon.querySelector(".tarea-estado").textContent = t.completada ? "Completada" : "Pendiente";
+
+        clon.querySelector(".btn-estado").onclick = () => cambiarEstado(t.id, t.titulo, t.materia, t.completada);
+        clon.querySelector(".btn-eliminar").onclick = () => eliminar(t.id);
+        
+        lista.appendChild(clon);
     });
 }
 
